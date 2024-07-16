ec867466e1eda32483b3bdfc7495098d14e43f9c
Tabishbostami <tabish.bostami042000@gmail.com>
2024-07-16 13:30:05 +0530

    joke_application

---
+++ day2/backend/server.js
@@ -0,0 +1,46 @@
+// const express = require('express')
+import express from "express";
+
+const app = express();
+
+// app.get("/", (req, res) => {
+//   res.send("server is ready");
+// });
+
+// list of five jokes
+app.get("/api/jokes", (req, res) => {
+  const jokes = [
+    {
+      id: 1,
+      title: "Why don't scientists trust atoms?",
+      content: "Because they make up everything!",
+    },
+    {
+      id: 2,
+      title: "How do you organize a space party?",
+      content: "You planet.",
+    },
+    {
+      id: 3,
+      title: "Why did the scarecrow win an award?",
+      content: "Because he was outstanding in his field!",
+    },
+    {
+      id: 4,
+      title: "What do you call fake spaghetti?",
+      content: "An impasta.",
+    },
+    {
+      id: 5,
+      title: "Why don't skeletons fight each other?",
+      content: "They don't have the guts.",
+    },
+  ];
+  res.send(jokes);
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server at http://localhost:${port}`);
+});
